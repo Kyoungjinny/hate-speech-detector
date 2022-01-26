034f9807b3e855a1955f2129b7f089d2b5ed1f06
Kyoungjin <jinnymini@gmail.com>
2022-01-26 14:31:01 +0900

    Add detector api & refactor

--- api/loadModel.py
+++ api/loadModel.py
@@ -1,41 +1,52 @@
-#import joblib
 import numpy as np
+import pickle
+import re
+from konlpy.tag import Okt
 from tensorflow.keras.models import load_model
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.metrics import classification_report
-#from .prep import export
-#import os.path
-#import sys
-#file_dir = os.path.dirname(__file__)
-#sys.path.append(file_dir)
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+def confusionMatrix(inputSentence):
+    X_test = np.load('model/X_test.npy')
+    y_test = np.load('model/y_test.npy')
 
-def confusionMatrix():
-    X_test = np.load('X_test.npy')
-    y_test = np.load('y_test.npy')
-    #y_train = np.load('y_train.npy')
-
-    loaded_model = load_model('lstm_model.h5', compile=False)
+    #loaded_model = load_model('lstm_model.h5', compile=False)
+    loaded_model = pickle.load(open("model/lstm_model.pkl", 'rb'))
     predictions = loaded_model.predict(X_test)
     y_pred = (predictions > 0.5)
 
     a = accuracy_score(y_test, y_pred)
-    print(a)
     p = precision_score(y_test, y_pred)
-    #print(p)
     r = recall_score(y_test, y_pred)
-    #print(r)
     f1 = f1_score(y_test, y_pred)
-    #print(f1)
-
     #print(classification_report(y_test, y_pred, target_names=['hate', 'none']))
-
     score = []
+    inputResult = sentencePredict(inputSentence, loaded_model)
+    score.append(inputResult)
     score.append(a)
     score.append(p)
     score.append(r)
     score.append(f1)
     return score
+
+def sentencePredict(inputSentence, model):
+    okt = Okt()
+    stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다']
+    tokenizer = Tokenizer()
+    new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]', '', inputSentence)
+    new_sentence = okt.morphs(new_sentence, stem=True)
+    new_sentence = [word for word in new_sentence if not word in stopwords]
+    encoded = tokenizer.texts_to_sequences([new_sentence])
+    pad_new = pad_sequences(encoded, maxlen=30)
+    pred = float(model.predict(pad_new))  # 예측
+    print("pred : ", pred)
+    if (pred > 0.5):
+        print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(pred * 100))
+        return 1
+    else:
+        print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - pred) * 100))
+        return 0
 
 if __name__ == '__main__':
     cm = confusionMatrix()
